6c8b9cd5e674f325b96ac4f5cc03011e6a1a5356
Sagar Sharma <tech.sagarsh@gmail.com>
2023-08-20 21:18:14 +0530

    bit manipulation program adding

---
+++ Concept Based Questions/Bitwise/bitManipulation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> manipulateBit(int num, int i) {
+    vector<int> n;
+    int mask;
+    n.push_back(num>>(i-1)&1);
+    mask = (1<<(i-1));
+    if(n[0]){
+        n.push_back(num);
+        n.push_back(num&(~mask));
+    }else {
+        n.push_back(num|mask);
+        n.push_back(num);
+    }
+    return n;
+}
+int main() {
+    for(auto el: manipulateBit(25, 3)){
+        cout << el << "\t";
+    }
+}
